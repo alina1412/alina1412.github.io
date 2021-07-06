4983996ebe65a1ab9942808f395b1a2a40a18ddd
alina1412 <alina1412@mail.ru>
2021-07-06 18:42:20 +0300

    add img

--- js/script.js
+++ js/script.js
@@ -18,11 +18,11 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.setBaseURL('http://labs.phaser.io'); 
+    this.load.setBaseURL('../img'); 
 
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image('sky', 'space3.png');
+    this.load.image('logo', 'phaser3-logo.png');
+    this.load.image('red', 'red.png');
 }
 
 function create()
