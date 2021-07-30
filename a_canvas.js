c0f621729690db7b4ab8890a39f3095d86c53562
alina1412 <alina1412@mail.ru>
2021-07-30 09:06:35 +0300

    canvas draw

--- a_canvas.js
+++ a_canvas.js
@@ -43,6 +43,55 @@ function change_rec_location(rec_parameters) {
 
 }
 
+
+// 
+
+
+let button2 = document.querySelector("#clear");
+// button2.addEventListener("click", () => {
+//     clear(canv);
+//     console.log("Button Clear clicked.");
+// });
+function clear(ctx, canvas) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function draw_a(ctx, canv) {
+
+    let painting = false;
+
+    function startPosistion(e) {
+        painting = true;
+        draw2(e);
+    }
+    function endPosistion() {
+        painting = false;
+        ctx.beginPath();
+    }
+    function draw2(e) {
+        if(!painting) return;
+        ctx.lineWidth = 8;
+        ctx.lineCap = 'round';
+        ctx.fillStyle = "rebeccapurple";
+
+        ctx.lineTo(e.clientX, e.clientY);
+        ctx.stroke();
+        ctx.beginPath();
+
+        ctx.arc(e.clientX, e.clientY, 10, 0, Math.PI * 2);
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.moveTo(e.clientX, e.clientY);
+    }
+
+    canv.addEventListener('mousedown', startPosistion);
+    canv.addEventListener('mouseup', endPosistion);
+    canv.addEventListener('mousemove', draw2);
+ 
+}
+
+
 (function() {
 
     // console.log(config.defaults.name2);
@@ -58,6 +107,14 @@ function change_rec_location(rec_parameters) {
     // };
     //draw_rec(ctx, rec_parameters);
     run_changes(ctx);
+    var canv = document.getElementById('canvas001');
+    draw_a(ctx, canv);
 
+    button2.onclick = function(ctx, canvas) {
+        clear(ctx, canvas);
+      };
 
 })();
+
+
+
